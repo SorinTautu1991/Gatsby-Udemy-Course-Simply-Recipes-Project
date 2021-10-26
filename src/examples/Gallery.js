36d31206bc8f86eeea4b50a4020cdb9242a8f684
SorinTautu1991 <sorin.tautu@gmail.com>
2021-10-26 21:55:26 +0300

    Rudimentar way of rendering a GatsbyImage

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,31 +1,63 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
+import styled from "styled-components";
 
 const Gallery = () => {
   const data = useStaticQuery(graphql`
     {
-      allFile(filter: { extension: { ne: ".svg" } }) {
+      allFile(filter: { extension: { ne: "svg" } }) {
         nodes {
           name
           childImageSharp {
             gatsbyImageData(
               layout: FIXED
               placeholder: BLURRED
-              transformOptions: { grayscale: true }
+              width: 200
+              height: 200
             )
           }
         }
       }
     }
   `);
-  console.log(data);
+
+  const {
+    allFile: { nodes },
+  } = data;
+  console.log(nodes);
 
   return (
-    <div>
-      <h2>Simple image gallery</h2>
-    </div>
+    <Wrapper>
+      {nodes.map((img, index) => {
+        const {
+          name,
+          childImageSharp: { gatsbyImageData },
+        } = img;
+        return (
+          <article className="item" key={index}>
+            <GatsbyImage
+              className="gallery-img"
+              image={gatsbyImageData}
+              alt={name}
+            />
+            <p>{name}</p>
+          </article>
+        );
+      })}
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin-right: 1rem;
+  }
+  .gallery-img {
+    border-radius: 1rem;
+  }
+`;
 
 export default Gallery;
